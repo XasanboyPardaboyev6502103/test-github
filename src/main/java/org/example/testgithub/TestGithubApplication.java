73a9f049573823fcd23e0a165b632b6f9f810fb0
Ozodbek <ozod80785@gmail.com>
2025-09-05 10:56:45 +0500

    ozodbek first command

--- src/main/java/org/example/testgithub/TestGithubApplication.java
+++ src/main/java/org/example/testgithub/TestGithubApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestGithubApplication {
 
+    System.out.print("hi");
+
     public static void main(String[] args) {
         SpringApplication.run(TestGithubApplication.class, args);
     }
