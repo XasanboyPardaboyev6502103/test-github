538c42d00a338a1616c3bd72bf057f048346c49f
Ozodbek <ozod80785@gmail.com>
2025-09-05 11:44:46 +0500

    birinchi sayt

--- src/main/java/org/example/testgithub/TestGithubApplication.java
+++ src/main/java/org/example/testgithub/TestGithubApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestGithubApplication {
 
-    System.out.print("hiwdwewed");
+    
 
     public static void main(String[] args) {
         SpringApplication.run(TestGithubApplication.class, args);
     }
+    System.out.print("Ozodbek");
 
 }
