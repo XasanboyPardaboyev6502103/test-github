fec39290776d2eb322d78e8cb143d51009a8a4bf
xasanboyuzdi2325yeie4225 <yeie4225>
2025-09-05 10:45:22 +0500

    empty project v1

---
+++ src/test/java/org/example/testgithub/TestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
